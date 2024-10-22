823ed107d98602d0c29e44a46f33b4103261ccc2
hugo cruz <hugocruz@hugos-MacBook-Pro.local>
2024-10-22 20:35:53 -0300

    basic structure for routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+@app.route("/balance", methods=['GET'])
+def balance():
+    return "balance"
+
+
+@app.route("/event", methods=['POST'])
+def event():
+    return "event"
